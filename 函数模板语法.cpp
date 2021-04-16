d6cb9adda6a8a6711407a38308e5dbfe11dd5630
MasterMaxLi <riwinkids@gmail.com>
2021-04-16 21:29:24 +0800

    first commit

--- 函数模板语法.cpp
+++ 函数模板语法.cpp
@@ -1,6 +1,22 @@
 ﻿#include <iostream>
 
+using namespace std;
+
+//函数模板
+template<typename T>    //声明模板，T是一个通用数据类型
+void mySwap(T& a, T& b)
+{
+    T temp = a;
+    a = b;
+    b = temp;
+}
+
 int main()
 {
-    std::cout << "Hello World!\n";
+    int a = 10;
+    int b = 20;
+    mySwap<int>(a, b);
+    cout << "a=" << a << endl;
+    cout << "b=" << b << endl;
+    return 0;
 }
